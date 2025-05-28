25995c637ffc65997cf5c277b50ea707a4b6e423
Rosali Johansson <Rosali.alfta@gmail.com>
2025-05-28 11:44:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'login.html'),
-        adminGallery: resolve(__dirname, 'adminGallery.html'),
-        adminMenu: resolve(__dirname, 'adminMenu.html'),
-        adminOrders: resolve(__dirname, 'adminOrders.html'),
+        adminGallery: resolve(__dirname, 'admingallery.html'),
+        adminMenu: resolve(__dirname, 'adminmenu.html'),
+        adminOrders: resolve(__dirname, 'adminorders.html'),
       }
     }
   }
